8394e9646c73de2a7f6752ceb934b507715e4187
João Amaro <joaovictoramaroalves@gmail.com>
2025-08-21 16:16:12 -0300

    Get game by ID

--- src/main/java/com/Dslist/Dslist/controller/GameController.java
+++ src/main/java/com/Dslist/Dslist/controller/GameController.java
@@ -1,10 +1,11 @@
 package com.Dslist.Dslist.controller;
 
+import com.Dslist.Dslist.dto.GameDTO;
 import com.Dslist.Dslist.dto.GameMinDTO;
-import com.Dslist.Dslist.entities.Game;
 import com.Dslist.Dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/Dslist/Dslist/service/GameService.java
+++ src/main/java/com/Dslist/Dslist/service/GameService.java
@@ -1,8 +1,10 @@
 package com.Dslist.Dslist.service;
 
+import com.Dslist.Dslist.dto.GameDTO;
 import com.Dslist.Dslist.dto.GameMinDTO;
 import com.Dslist.Dslist.entities.Game;
 import com.Dslist.Dslist.repository.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,16 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public List<GameMinDTO> findAll(){
+    @Transactional
+    public List<GameMinDTO> findAll() {
         List<Game> gameList = gameRepository.findAll();
-        return  gameList.stream().map(x -> new GameMinDTO(x)).toList();
+        return gameList.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional
+    public GameDTO findById(long id) {
+        Game result = gameRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Game not found !"));
+        return new GameDTO(result);
     }
 }
